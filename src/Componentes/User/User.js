182b9f2dcc70f7ce9835ea39bf2e8ad2ccda1338
Bruna Pereira <perasbruna@gmail.com>
2022-05-12 12:09:00 -0300

    final

--- src/Componentes/User/User.js
+++ src/Componentes/User/User.js
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import UserHeader from './UserHeader';
 import Feed from '../Feed/Feed';
 import UserPhotoPost from './UserPhotoPost';
-import UserStats from './UserStats';
 import { UserContext } from '../../UserContext';
 import NotFound from '../NotFound';
 import Head from '../Helper/Head';
@@ -18,7 +17,6 @@ const User = () => {
       <Routes>
         <Route path='/' element={<Feed user={data.id} />} />
         <Route path='/postar' element={<UserPhotoPost/>} />
-        <Route path='estatisticas' element={<UserStats/>} />
         <Route path='*' element={<NotFound/>} />
       </Routes>
     </section>
--- src/Componentes/User/UserHeaderNav.js
+++ src/Componentes/User/UserHeaderNav.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
 import { UserContext } from '../../UserContext';
 import {ReactComponent as MinhasFotos} from '../../Assets/feed.svg';
-import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg';
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg';
 import {ReactComponent as Sair} from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
@@ -23,7 +22,6 @@ const UserHeaderNav = () => {
     {mobile && <button aria-label='Menu' className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`} onClick={() => setMobileMenu(!mobileMenu)}></button>}
     <nav className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}>
       <NavLink to='/conta' end><MinhasFotos/>{mobile && 'Minhas Fotos'}</NavLink>
-      <NavLink to='/conta/estatisticas'><Estatisticas/>{mobile && 'Estatísticas'}</NavLink>
       <NavLink to='/conta/postar'><AdicionarFoto/>{mobile && 'Adicionar Foto'}</NavLink>
       <button onClick={userLogout}>{''}<Sair/>{mobile && 'Sair'}</button>
     </nav>
